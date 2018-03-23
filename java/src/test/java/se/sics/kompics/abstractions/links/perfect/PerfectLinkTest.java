546913b1a26b15436cabd1487559fbfa819e8c21
Max Meldrum <max@meldrum.se>
2018-03-23 21:25:20 +0100

    Improve Pp2p test

--- java/src/test/java/se/sics/kompics/abstractions/links/perfect/PerfectLinkTest.java
+++ java/src/test/java/se/sics/kompics/abstractions/links/perfect/PerfectLinkTest.java
@@ -39,32 +39,31 @@ import static org.junit.Assert.assertTrue;
 
 public class PerfectLinkTest {
 
-    //TODO: Improve..
     @Test
     public void Pp2pSendTest() {
         NetAddress n1 = new NetAddress(InetAddress.getLoopbackAddress(), 12346);
         NetAddress n2 = new NetAddress(InetAddress.getLoopbackAddress(), 12347);
 
-        try {
-            TestContext<PerfectLink> tc = TestContext.newInstance(PerfectLink.class, new PerfectLink.Pp2pInit(n1));
-            Component comp = tc.getComponentUnderTest();
-            Negative<Network> net = comp.getNegative(Network.class);
-            Positive<PerfectLinkPort> pLink = comp.getPositive(PerfectLinkPort.class);
+        TestContext<PerfectLink> tc = TestContext.newInstance(PerfectLink.class, new PerfectLink.Pp2pInit(n1));
+        Component comp = tc.getComponentUnderTest();
+        Negative<Network> net = comp.getNegative(Network.class);
+        Positive<PerfectLinkPort> pLink = comp.getPositive(PerfectLinkPort.class);
 
-            TestEvent event = new TestEvent();
-            Pp2pSend pp2pSend = new Pp2pSend(n2, event);
+        TestEvent event = new TestEvent();
+        Pp2pSend pp2pSend = new Pp2pSend(n2, event);
 
-            tc.body()
-                    .trigger(pp2pSend, pLink)
-                    // Check that the message that is heading "out" has correct src and dst
-                    .expect(Message.class, (Message m) -> m.header.src.equals(n1) && m.header.dst.equals(n2),
-                            net, Direction.OUT);
+        tc.body()
+                .trigger(pp2pSend, pLink)
+                .expect(Message.class, (Message m) -> isValid(m, n1, n2), net, Direction.OUT);
 
-            assertTrue(tc.check());
+        assertTrue(tc.check());
+    }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    // Check that the message that is heading "out" has correct src, dst
+    // and that it contains a Pp2pDeliver payload
+    private boolean isValid(Message m, NetAddress src, NetAddress dst) {
+        boolean isDeliver = m.payload instanceof Pp2pDeliver;
+        return (isDeliver && m.header.src.equals(src) && m.header.dst.equals(dst));
     }
 
     private class TestEvent implements KompicsEvent {
